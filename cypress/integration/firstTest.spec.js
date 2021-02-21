c93c1700c8bf37edd89044898348a6f4c6f162a6
mockauer <barrus@gmx.de>
2021-02-21 18:31:02 +0100

    Add Cypress Config and first Test (find Elements)

---
+++ cypress/integration/firstTest.spec.js
@@ -0,0 +1,44 @@
+/// <reference types="cypress" />
+
+
+describe('Our first suite', () => {
+
+    it('First test', () => {
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Form Layouts').click()
+
+        //Seaching by locators, nicht xpath
+        
+        // by Tag Name
+        cy.get('input')
+
+        //by ID -> # sagt es ist ID
+        cy.get('#inputEmail1')
+
+        //by Classname --> .sagt es ist eine Klasse, auch nur die erste Klasse nehmen
+        cy.get('.input-full-width')
+
+        //by Attribute name
+        cy.get('[placeholder]')
+
+        //by Attribute name and value
+        cy.get('[placeholder="Email"]')
+
+        //by Class value
+        cy.get('[class="input-full-width size-medium shape-rectangle"]')
+
+        //by tag name and attribute with value
+        cy.get('input[placeholder="Email"]')
+
+        //by two different attributes
+        cy.get('[placeholder="Email"][fullwidth]')
+
+        //by tag name, attribute with value, id and class name
+        cy.get('input[placeholder="Email"]#inputEmail1.input-full-width')
+
+        //by most recommended way by cpress
+        cy.get('[data-cy="imputEmail1"]')
+    })
+
+})
