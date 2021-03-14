18ecda75c2f213e7b83a6c71419c42f2d9ffca1f
SL <38684838+mockauer@users.noreply.github.com>
2021-03-14 14:16:12 +0100

    Add Lists and Dropdowns, Radio and Checkooxen

--- cypress/integration/secondTest.spec.js
+++ cypress/integration/secondTest.spec.js
@@ -134,18 +134,104 @@ describe('Our first suite', () => {
  })
 
       //5 Invoke beim Datepicker
-      it.only('assert property', () =>{
+      it('assert property', () => {
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Datepicker').click()
+
+        cy.contains('nb-card', 'Common Datepicker').find('input').then( input => {
+          cy.wrap(input).click()
+          cy.get('nb-calendar-day-picker').contains('17').click()
+          cy.wrap(input).invoke('prop', 'value').should('contain', 'Mar 17, 2021')
+
+
+          // würde auch klappen cy.get(input).click()
+        })
+     })
+
+     //Radiobutton
+     it('radio button', () => {
       cy.visit('/')
       cy.contains('Forms').click()
-      cy.contains('Datepicker').click()
+      cy.contains('Form Layouts').click()
 
-      cy.contains('nb-card', 'Common Datepicker').find('input').then( input => {
-        cy.wrap(input).click()
-        cy.get('nb-calendar-day-picker').contains('17').click()
-        cy.wrap(input).invoke('prop', 'value').should('contain', 'Feb 17, 2021')
+      cy.contains('nb-card', 'Using the Grid').find('[type="radio"]').then( radioButtons => {
+        //JS Object, deshalb Wrap
+        //cy.wrap(radioButtons).first().check() // wirft so Fehler, weil die CSS Klasse hidden ist für DOM
+        cy.wrap(radioButtons).first().check({force: true})
+        .should('be.checked')
 
+        //Zweiter Radio button klicken und erster muss leer sein
+        cy.wrap(radioButtons).eq(1).check({force:true}) //eq is die Anzahl des Elements, bei mehreren
+        cy.wrap(radioButtons).first().should('not.be.checked')
 
-        // würde auch klappen cy.get(input).click()
+        
+        cy.wrap(radioButtons).eq(2).should('be.disabled')
+
       })
-      })
+     })
+
+
+     //checkboxes
+     it('check boxes', () => {
+      cy.visit('/')
+      cy.contains('Modal & Overlays').click()
+      cy.contains('Toastr').click()
+
+      //klickt alle Checkboxen, kann nicht unchecken
+      cy.get('[type="checkbox"').check({force: true})
+      cy.get('[type="checkbox"').eq(0).click({force: true}) //die erste wird checkbox wird demarkiert
+
+     })
+
+
+     //Lists and Dropdown
+     it.only('Lists and dropdown', () => {     
+        cy.visit('/')
+
+        // //Dark Theme auswählen
+        // cy.get('nav nb-select').click()
+        // cy.get('.options-list').contains('Dark').click()
+
+        // //schauen, ob das Dark Theme wirklich zusehen ist, Farbe ist in RGB zu mache nicht HEX Code
+        // cy.get('nav nb-select').should('have.text', ' Dark')
+        // cy.get('nav nb-select').should('contain.text', 'Dark') // das gleich wie should('contain', [TEXT])
+        // cy.get('nb-layout nav').should('have.css', 'background-color', 'rgb(34, 43, 69)')
+
+
+        //etwas komplizierter --> es wird durch alle durch iteriert
+        cy.get('nav nb-select').then( dropdown => {
+          cy.wrap(dropdown).click()
+          //each() gleiche Funktion wie in JS, Iterator --> wir wollen alle Wörter in der dropdown-Liste auslesen
+          //zweiter Parameter index für Condtion am Ende
+          cy.get('.options-list nb-option').each( (listItem, index) => {
+              const itemText = listItem.text().trim()
+              console.log(itemText)
+
+              const colors = {
+                "Light": "rgb(255, 255, 255)",
+                "Dark": "rgb(34, 43, 69)",
+                "Cosmic": "rgb(50, 50, 89)",
+                "Corporate": "rgb(255, 255, 255)"
+              }
+              console.log(colors)
+
+              //schauen ob Liste die Wörter enthält, die man gerade ausgelesen hat
+              cy.wrap(listItem).click()
+              cy.wrap(dropdown).should('contain', itemText) 
+              //das würde nur fürs erste Klappen wegen der Farbe  --> darum die Constante colors dazu
+              cy.get('nb-layout nav').should('have.css', 'background-color', colors[itemText])
+              //hier brauchen wir den index parameter, beim letzten mal nicht mehr clicken
+              if(index < 3) {
+                cy.wrap(dropdown).click()
+              }
+
+              //cy.select() --> wenn HTML Select mit Options ist --> Doku schauen 
+
+
+          })
+        })
+
+
+     })
 })
