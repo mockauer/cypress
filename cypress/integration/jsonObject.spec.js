82487179a35a3059d81fbeeacb788cc3e241b2d0
mockauer <barrus@gmx.de>
2022-02-16 12:41:18 +0100

    EDT: Add first API Test

--- cypress/integration/jsonObject.spec.js
+++ cypress/integration/jsonObject.spec.js
@@ -49,8 +49,38 @@ describe('JSON Object', () => {
     }
     )
 
-    it.only('Success Login', () => {
-      cy.log('YES WE CAN')
+    it.only('verify correct request and response', () => {
+
+      cy.server()
+      //as als alias für Zwischenspeicherung
+      cy.route('POST', '**/articles').as('postArticles')
+      //route geht nicht ohne server
+
+
+      //beginnen mit GUI Eintrag der Oberfläche
+      cy.contains('New Article').click()
+      cy.get('[formcontrolname="title"]').type("Article SL Cypress")
+      cy.get('[formcontrolname="description"]').type("Article SL Cypress")
+      cy.get('[formcontrolname="body"]').type("Article SL Cypress")
+      cy.contains('Publish Article').click()
+
+
+      //so jetzt wollen wir die API abfragen, ob es wirklich so ankommt > article
+      //zu erst über den Test den cy.server initialisieren und cy.route hören auf die Methode, aber sind veraltet, dafür dann intercept
+
+      //warten auf den Response 
+      //das @ ist wichtig für die Abfrage der Variable in einem .as (Alias)
+      cy.wait('@postArticles')
+      cy.get('@postArticles').then( xhr => {
+        console.log("HIER:")
+        console.log(xhr)
+        // expect(xhr.response.body.article.description)
+        expect(xhr.status).to.equal(200)
+        expect(xhr.request.body.article.body).to.equal("Article SL Cypress")
+        expect(xhr.response.body.article.description).to.equal("Article SL Cypress")
+      })
+
+
     })
 }
 )
